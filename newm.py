8b4a3db292d3757ffc78baf19f3978279b74f648
NikitaED <Nik.lesinc@yandex.ru>
2024-09-17 21:02:53 +0500

    newe

--- newm.py
+++ newm.py
@@ -1,10 +1,7 @@
 my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 start = 0
-
-while my_list[start] > 0 and len(my_list) > start:
+while my_list[start] >= 0 and len(my_list) > start:
     if my_list[start] == 0:
         start += 1
-    if my_list[start] < 0:
-        break
     print(my_list[start])
     start += 1
